5c39fc9c363f34dcf44f11601af348e2e381c394
David Pinheiro <davepinh@gmail.com>
2018-06-10 09:57:21 -0300

    Testing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+import Test.Hspec
+import Cpf
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Cpf.valid" $ do
+        it "checks if CPF is valid" $ do
+                     Cpf.valid [1,1,1,1,1,1,1,1,1,1,1] `shouldBe` False
+    describe "Cpf.valid" $ do
+        it "checks if CPF is valid" $ do
+                     Cpf.valid [8,8,6,8,1,8,9,2,1,6,9] `shouldBe` True
